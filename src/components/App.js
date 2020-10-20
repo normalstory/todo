e3ac8253e2fb0956635d9202ce4fe4406f557699
thinknormal <nornalstory@gmail.com>
2020-10-20 15:48:46 +0900

    3.상태관리 1)input text

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,23 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 export default class App extends Component{
+    state={
+        input:'' //input값 
+    }
+    handleChange=(e)=>{
+        const {value} =e.target;
+        
+        this.setState({
+            input:value
+        })
+    }
     render(){
+        const {input} =this.state;
+        const {handleChange}=this;
+
         return(
             <PageTemplate>
-                <TodoInput/>
+                <TodoInput onChange={handleChange} value={input}/>
                 <TodoList/>
             </PageTemplate>
         )
